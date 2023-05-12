26aa68998f6eb40c27c4d3cb2abef0388a4d3bf6
Gal Aviezri <g.aviezri@gmail.com>
2023-05-12 10:59:39 +0300

    powersource

---
+++ Garage/PowerSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal abstract class PowerSource
+    {
+        internal abstract void Fill(float i_Amount);
+    }
+}
