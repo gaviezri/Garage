465cebc04c2cf22c7016ad16308f70dc73174f89
Gal Aviezri <g.aviezri@gmail.com>
2023-05-12 11:44:30 +0300

    garageManager + VehicleInGarage

--- Garage/PetroleumPowerSource.cs
+++ Garage/PetroleumPowerSource.cs
@@ -2,24 +2,24 @@
 {
     internal class PetroleumPowerSource : PowerSource
     {
-        internal enum PetrolType
+        internal enum ePetrolType
         {
             Soler, Octan95, Octan96, Octan98
         }
         
         private float m_RemainingTank;
-        private readonly float m_MaximumTank;
-        private PetrolType m_Type;
+        private readonly float r_MaximumTank;
+        private ePetrolType m_Type;
 
-        internal PetroleumPowerSource(float i_RemainingTank, float i_MaximumTank, PetrolType i_Type)
+        internal PetroleumPowerSource(float i_RemainingTank, float i_MaximumTank, ePetrolType i_Type)
         {
             m_RemainingTank = i_RemainingTank;
-            m_MaximumTank = i_MaximumTank;
+            r_MaximumTank = i_MaximumTank;
             m_Type = i_Type;
         }
         internal override void Fill(float i_Amount)
         {
-            if(AmountInRange(i_Amount, m_RemainingTank, m_MaximumTank))
+            if(AmountInRange(i_Amount, m_RemainingTank, r_MaximumTank))
             {
                 m_RemainingTank += i_Amount;
             }
