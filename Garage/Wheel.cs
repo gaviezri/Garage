1612b16e6104d54686893d3f2cb421c93684f46a
Gal Aviezri <g.aviezri@gmail.com>
2023-05-12 10:52:38 +0300

    wheel - done

---
+++ Garage/Wheel.cs
@@ -0,0 +1,26 @@
+﻿namespace Garage
+{
+    internal class Wheel
+    {
+        private string m_Manufacturer;
+        private float m_CurrentPressure;
+        private float m_MaximumPressure;
+
+        internal Wheel(string i_Manufacturer, float i_CurrentPressure, float i_MaximumPressure)
+        {
+            m_Manufacturer = i_Manufacturer;
+            m_CurrentPressure = i_CurrentPressure;
+            m_MaximumPressure = i_MaximumPressure;  
+        }
+
+        internal void Inflate(float i_Amount)
+        {
+            if (i_Amount < 0 || (i_Amount + m_CurrentPressure > m_MaximumPressure) )
+            {
+                // throw new ValueOutOfRangeException("...");
+            }
+
+        }
+        
+    }
+}
